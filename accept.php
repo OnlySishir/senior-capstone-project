b311cb6283e674010a437204fcbd6a800c44de90
Nadia313 <55292953+Nadia313@users.noreply.github.com>
2020-02-01 15:41:35 -0500

    Add files via upload

---
+++ accept.php
@@ -0,0 +1,31 @@
+<?php
+    include('functions.php');
+    $id = $_GET['id'];
+    $query = "select * from `requests` where `id` = '$id';";
+    if(count(fetchAll($query)) > 0){
+        foreach(fetchAll($query) as $row){
+            $firstname = $row['firstname'];
+            $lastname = $row['lastname'];
+            $email = $row['email'];
+            $password = $row['password'];
+            $confirmpassword = $row['confirmpassword'];
+            $postalcode = $row['postalcode'];
+            $type = $row['type'];
+            $link = $row['link'];
+            $info = $row['info'];
+            $msg = "First line of text\nSecond line of text";
+            $query = "INSERT INTO `accounts` (`firstname`, `lastname`, `email`,`password`, `confirmpassword` , `postalcode`, `type`, `link`, `info`) VALUES ('$firstname', '$lastname', '$email', '$password', '$confirmpassword', '$postalcode', '$type', '$link', '$info');";
+        }
+        $query .= "DELETE FROM `requests` WHERE `requests`.`id` = '$id';";
+        if(performQuery($query)){
+            echo "Account has been accepted.";
+        }else{
+            echo "Unknown error occured. Please try again.";
+        }
+    }else{
+        echo "Error occured.";
+    }
+    
+?>
+<br/><br/>
+<a href="home.php">Back</a>
