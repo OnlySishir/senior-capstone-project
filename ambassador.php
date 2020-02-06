b7e8ac661dab2e1424113eecf0c6ce8d6bedc47b
Nadia313 <55292953+Nadia313@users.noreply.github.com>
2020-02-06 03:51:09 -0500

    working on ambassador page

---
+++ ambassador.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Become An Ambassador</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+
+    <!-- Bootstrap CSS -->
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    <!-- Main Stylesheet File -->
+    <link href="css/style.css" rel="stylesheet" />
+  </head>
+  <body>
+<header>
+    <!-- Nav Bar Start -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="index.html">
+        <img src="./img/EC_logo-Black.png" alt="Logo" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarsExample03"
+        aria-controls="navbarsExample03"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarsExample03">
+        <form class="form-inline my-2 my-md-0">
+          <input class="form-control" type="text" placeholder="Search" />
+        </form>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="dropdown03"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+              >Shop
+            </a>
+            <div class="dropdown-menu" aria-labelledby="dropdown03">
+              <a class="dropdown-item" href="#">Shop by Category</a>
+              <a class="dropdown-item" href="#">Shop by Artist</a>
+              <a class="dropdown-item" href="#">Shop by Location</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="signup.php">Join as an Artist/Designer</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Log In</a>
+          </li>
+          <a class="btn btn-outline-dark" href="signup.php" role="button">
+                Sign Up Free</a>
+          
+        </ul>
+      </div>
+    </nav>
+    <!-- Nav Bar End -->
+
+</header>
+
+    <div class="text2">
+    <h2>Become a Digital Ambassador to Help Strengthen Your Creative Community.</h2>
+    <br>
+    <h4>We know why you&#8217;re here on Embrace Creatives. Well, yeah, you want to generate professional relationships and revenue but it&#8217;s more than that&#8230;It&#8217;s because you realize how important it is to expose artists and art-centric business owners to creative empowerment like;</h4>
+<br>
+    <ul><li>skill building education that brings confidence, </li><li>powerful connections that boost one&#8217;s support group, and </li><li>sales opportunities that one wouldn&#8217;t be able to secure on their own.</li></ul>
+
+
+
+<p>Artists want MORE than sales, they also crave ways to bring positive change to their art community because they know, first hand that many artists need support.</p>
+
+
+
+<ul><li>Artists are insular and lack access to peer support.</li><li>Artists have a difficult time connecting with Commercial Buyers, such as galleries, retail buyers, art reps, and interior designers.</li><li>The majority of Artists are missing business skills so are ill-equipped to succeed.</li></ul>
+
+<p><strong>Embrace Creative is focused on Creative Empowerment and we&#8217;re growing our community in a grassroots way, organically (people telling people) but we can use help.</strong></p>
+
+
+
+<h4>Being an Embrace Creatives&#8217; Digital Ambassador will allow you to easily uplift and engage your peers through your social media platforms.</h4>
+
+
+
+<p>Although <strong>we welcome everyone to promote our cause</strong>, you <strong>must be an Embrace Creatives&#8217; Member </strong>to become a Digital Ambassador<strong> so <a href="signup.php">join us</a></strong>! (<em>it&#8217;s free and VERY beneficial to growing your own art business</em>)</p>
+<br>
+<br>
+<center><h4><strong>How It Works</strong></h4></center>
+
+<center><img src="https://embracecreatives.com/wp-content/uploads/2020/01/APPLY-Ambassador-blog.png" alt=""></center>
+
+<br>
+<br>
+<center><h4><strong>Watch the Digital Ambassador Guide video!</strong></h4></center>
+<br>
+<br>
+<center><p><iframe title="Embrace Creatives Digital Ambassador Guide" width="80%" height="550" src="https://www.youtube.com/embed/rxz-T6_zyqk?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe></p></center>
+<br>
+
+    <center><h4><strong>EC DIGITAL AMBASSADOR PERKS</strong></h4></center>
+
+    <center><img src="https://embracecreatives.com/wp-content/uploads/2020/01/APPLY-Ambassador-blog1.png" alt=""></center>
+
+    <br><h4>Digital Ambassador Sign Up</h4>
+
+    <br>
+    
+    <div class="form-group">
+
+    <strong><label class="control-label" for="name"><p>Name</p></label></strong><span class="required">*</span>    
+
+    <br><input type="text" id="name" name="data[name]" required="required" class="form-control">
+    <p>First</p>
+
+    <input type="text" id="name" name="data[name]" required="required" class="form-control">
+    <p>Last</p>
+
+    <strong><label class="control-label" for="name"><p>Are you an Embrace Creatives&#039; Member?</p></label></strong><span class="required">*</span>
+<br>
+
+<select class="browser-default custom-select custom-select-nm mb-3" style= width:100% name="type" >
+  <option selected>Options</option>
+  <option value="Yes">Yes! 😀</option>
+  <option value="NO">No</option>
+  <option value="future">I'll join now to become an official Digital Ambassador</option>
+</select>
+
+<p>Although everyone can share about our important cause, only EC Members can be official Digital Ambassadors so if you're not in our community, JOIN US - It's FREE! Copy and paste: www.embracecreatives.com/join -  then come back to this page.</p>
+
+<button name="submit" class="btn btn-sm btn-primary btn-block" style= width:100px type="submit">Submit</button>
+
+
+</div> 
+</div>
+
+
+<!-- Footer Start -->
+<div class="footer">
+  <div class="contain">
+    <div class="col">
+      <h1>ABOUT</h1>
+      <ul>
+        <li><a href="about.html" class="">About EC</a></li>
+        <li><a href="contact.html">Contact Us</a></li>
+        <li><a href="#">Become an Ambassador</a></li>
+      </ul>
+    </div>
+    <div class="col">
+      <h1>Company</h1>
+      <ul>
+        <li><a href="terms and conditions.html" class="">Privacy Policy</a></li>
+        <li><a href="terms and conditions.html" class="">Terms & Conditions</a></li>
+        <li><a href="Community Guidlines.html" class="">Community Guidelines</a></li>
+        <li><a href="#" class="">Report Abuse</a></li>
+      </ul>
+    </div>
+    <div class="col">
+      <h1>Education</h1>
+      <ul>
+        <li><a href="blog-home.html" class="">Blog</a></li>
+        <li><a href="#" class="">EC User Manual</a></li>
+      </ul>
+    </div>
+    <div class="col">
+      <h1>Inspiration</h1>
+      <ul>
+        <li><a href="#" class="">Art & Design Market</a></li>
+      </ul>
+    </div>
+    <div class="col">
+      <h1>Connections</h1>
+      <ul>
+        <li><a href="#" class="">Members</a></li>
+        <li><a href="#" class="">Events</a></li>
+      </ul>
+    </div>
+    <div class="col">
+      <h1>Opportunities</h1>
+      <ul>
+        <li><a href="#" class="">Art Calls</a></li>
+        <li><a href="#" class="">Grants</a></li>
+        <li><a href="#" class="">Advertise</a><li>
+      </ul>
+    </div>
+    <div class="clearfix"> </div>
+  </div>
+</div>
+<br>
+<div class="container">
+  <div class="row justify-content-md-center">
+    <div class="copyright-footer">
+      <p class="copyright color-text-a">
+        &copy; Copyright
+        <span class="color-a"></span> 2020 Embrace Creatives, LLC all rights reserved.Embrace Creatives is a women-owned business headquartered in metro Detroit.
+      </p>
+    </div>
+  </div>
+</div>
+<!-- Footer End -->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script
+      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
