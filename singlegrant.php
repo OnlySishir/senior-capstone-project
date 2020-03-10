65e05f1696fee85fbb4fe99f397b828cc16750cf
Nadia313 <55292953+Nadia313@users.noreply.github.com>
2020-03-10 01:30:51 -0400

    grant detail page

---
+++ singlegrant.php
@@ -0,0 +1,54 @@
+<style>
+
+.masthead {
+  height: 60vh;
+  min-height: 100px;
+  background-image: url("https://www.gstatic.com/webp/gallery/4.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+</style>
+<br>
+<div class="text" style="text-align: left;">
+<h2>Title of the grant</h2>
+<br>
+</div>
+<header class="masthead">
+  <div class="container h-100">
+    <div class="row h-100 align-items-center">
+</div>
+  </div>
+</header>
+
+<!-- Page Content -->
+<div class="text" style="text-align: right;">
+<p>Posted on (Date) by user first name and last name</p>
+</div>
+<section class="py-5">
+  <div class="container">
+
+  <div class="text1" style="text-align: left;">
+   <div>
+									<p><strong>Category:</strong> Grant</p>
+								<p><strong>Open To:</strong> Country-only submissions</p>
+				<p><strong>Who Should Apply?</strong>  visual and performing artists</p>
+				<p><strong>Application Deadline:</strong> Date</p>
+				<p><strong>Grant Amount/Range:</strong> between $200 and $2,500</p>
+				<p><strong>Application Fee:</strong> </p>
+            </div>
+            <h4>Summary</h4>
+			<p>**IS THIS OPPORTUNITY NOT THE RIGHT FIT? SHARE BELOW TO EMPOWER OTHERS!**<br />
+Deadline Date: Rolling Do you have an unanticipated opportunity to present your work? Did you incur an unexpected expense that you didn’t budget for? The Foundation for Contemporary Arts offers Emergency Grants between $200 and $2,500 for visual and performing artists. They review applications once a month, so you can quickly take advantage of momentum or solve any budget errors. </p>
+
+<h4>How to Apply</h4>
+			See website for more details and to apply.			<div>
+				<p><strong>Contact:</strong> </p>
+								<p><strong>Website:</strong> <a href="https://www.foundationforcontemporaryarts.org/grants/emergency-grants">https://www.foundationforcontemporaryarts.org/grants/emergency-grants</a></p>
+			</div>
+		</div>
+
+</div>
+</div>
+</section>
