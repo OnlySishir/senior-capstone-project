41042159a8bfcdad2c6e4ecb668f248968a7c98b
Nadia313 <55292953+Nadia313@users.noreply.github.com>
2020-03-25 19:01:25 -0400

    Updated grant list page.

--- grantlist.php
+++ grantlist.php
@@ -1,48 +1,15 @@
 <br>
+<br>
 
-<div class="page-right-sidebar">
 <div class="text" style="text-align: left;">
-	<header class="archive-header">
-	</header>
-	<section id="primary" class="site-content">
-		<div id="content" role="main">
 			
-			<div class="post-wrap">
-			<header>
-				<h1 class="entry-title">
-					<a href="https://embracecreatives.com/grants/foundation-for-contemporary-arts-emergency-grant/" rel="bookmark">Foundation for Contemporary Arts Emergency Grant</a>
+				<h1>
+					<a href="https://embracecreatives.com/grants/foundation-for-contemporary-arts-emergency-grant/">Foundation for Contemporary Arts Emergency Grant</a>
 				</h1>
-			</header>
-			<div class="entry-content entry-summary">
 									<p><strong>Category:</strong> Grant</p>
 								<p><strong>Amount:</strong> between $200 and $2,500</p>
 				<p><strong>Open To:</strong> Entries from United States (US) only</p>
 				<p><strong>Deadline:</strong> Date</p>
-			</div>
-		</div>
-        </div>
-        </div>
+</div>
         <hr width="90%">
 
-        <div class="page-right-sidebar">
-<div class="text" style="text-align: left;">
-	<header class="archive-header">
-	</header>
-	<section id="primary" class="site-content">
-		<div id="content" role="main">
-			
-			<div class="post-wrap">
-			<header>
-				<h1 class="entry-title">
-					<a href="https://embracecreatives.com/grants/foundation-for-contemporary-arts-emergency-grant/" rel="bookmark">Foundation for Contemporary Arts Emergency Grant</a>
-				</h1>
-			</header>
-			<div class="entry-content entry-summary">
-									<p><strong>Category:</strong> Grant</p>
-								<p><strong>Amount:</strong> between $200 and $2,500</p>
-				<p><strong>Open To:</strong> Entries from United States (US) only</p>
-				<p><strong>Deadline:</strong> Date</p>
-			</div>
-		</div>
-        </div>
-        </div>
