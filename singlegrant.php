b86333b6599f602d10545345ff393b7cd8178553
Nadia313 <55292953+Nadia313@users.noreply.github.com>
2020-03-25 23:27:52 -0400

    Single Grant display page

--- singlegrant.php
+++ singlegrant.php
@@ -1,51 +1,35 @@
-<style>
 
-.masthead {
-  height: 60vh;
-  min-height: 100px;
-  background-image: url("https://www.gstatic.com/webp/gallery/4.jpg");
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-}
-
-</style>
 <br>
 <div class="text" style="text-align: left;">
-<h2>Title of the grant</h2>
+<h2><?= $single['title']?></h2>
 <br>
 </div>
-<header class="masthead">
-  <div class="container h-100">
-    <div class="row h-100 align-items-center">
-</div>
-  </div>
-</header>
+    <img src="<?= base_url('upload/').$single['image']?>" class="img-fluid" alt="Responsive image" />
+
 
 <!-- Page Content -->
 <div class="text" style="text-align: right;">
-<p>Posted on (Date) by user first name and last name</p>
+<h6><span class="date">Posted on <?= date('Y-m-d', strtotime($single['regdate']))?>&nbsp;&nbsp;&nbsp;</span><span class="text-info">by:</span> <?= $single['firstname'].' '.$single['lastname']?>&nbsp;&nbsp;</h6>
 </div>
 <section class="py-5">
   <div class="container">
 
   <div class="text1" style="text-align: left;">
    <div>
-									<p><strong>Category:</strong> Grant</p>
-								<p><strong>Open To:</strong> Country-only submissions</p>
-				<p><strong>Who Should Apply?</strong>  visual and performing artists</p>
-				<p><strong>Application Deadline:</strong> Date</p>
-				<p><strong>Grant Amount/Range:</strong> between $200 and $2,500</p>
-				<p><strong>Application Fee:</strong> </p>
+									<p><strong>Category: </strong>Grant</p>
+								<p><strong>Open To:  </strong><?= $single['open_to']?></p>
+				<p><strong>Who Should Apply:  </strong><?= $single['who_apply']?></p>
+				<p><strong>Application Deadline:  </strong><?= $single['deadline']?></p>
+				<p><strong>Grant Amount/Range:  </strong><?= $single['amount']?></p>
+				<p><strong>Application Fee: </strong><?= $single['fee']?></p>
             </div>
-            <h4>Summary</h4>
-			<p>**IS THIS OPPORTUNITY NOT THE RIGHT FIT? SHARE BELOW TO EMPOWER OTHERS!**<br />
-Deadline Date: Rolling Do you have an unanticipated opportunity to present your work? Did you incur an unexpected expense that you didn’t budget for? The Foundation for Contemporary Arts offers Emergency Grants between $200 and $2,500 for visual and performing artists. They review applications once a month, so you can quickly take advantage of momentum or solve any budget errors. </p>
+            <h5>Summary</h5>
+			<p><?= $single['summary']?></p>
 
-<h4>How to Apply</h4>
-			See website for more details and to apply.			<div>
+<h5>How to Apply</h5>
+<?= $single['how_apply']?>			<div>
 				<p><strong>Contact:</strong> </p>
-								<p><strong>Website:</strong> <a href="https://www.foundationforcontemporaryarts.org/grants/emergency-grants">https://www.foundationforcontemporaryarts.org/grants/emergency-grants</a></p>
+								<p><strong>Website:  </strong> <a href="<?= $single['website']?>">&nbsp;&nbsp;<?php echo $single['website']?></a></p>
 			</div>
 		</div>
 
