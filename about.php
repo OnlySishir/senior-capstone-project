6b2a66bd2ab0f98f683339a3bd115969229d3883
Nadia313 <55292953+Nadia313@users.noreply.github.com>
2020-04-07 18:06:14 -0400

    Updated about page

---
+++ about.php
@@ -0,0 +1,139 @@
+<br>
+<div class="text2" style="text-align: left;">
+        <!-- Page starts -->
+  <h1>Welcome to Embrace Creatives</h1>
+  <br>
+  <h2>Our Story</h2>
+  <br>
+<p>Seeing the need for a B2B networking platform for artists and industry professionals, in June of 2018 Andrea Bogart, artist and business expert launched 
+Embrace Creatives, a caring, business ecosystem uniting artists and commercial buyers.  Soon after, artists, designers, galleries, business experts and non-
+profits from around the country began to join showcasing their art, finding opportunities and connecting with their peers and buyers.</p>
+
+<h4>Then something amazing happened.</h4>
+
+<p>People started asking, “How can I help you?” </p>
+
+<p></p>When enough people signed up to become a part of her exciting vision, Bogart realized that she had tapped into something bigger than she had expected. 
+Of course Artists want to sell and learn business skills but they desire more than that. Artists crave community, peer support and a clear way to affect positive change in their community. 
+So using the scale of an online network paired with in-person and virtual events, Andrea is igniting an empowerment movement for Artists through her Embrace Creatives ecosystem.</p>
+
+<p>Andrea, her Champions, and Chapter Directors are thoughtfully developing a grassroots movement to strengthen our art business ecosystem. 
+EC is mindfully growing from city-to-city, state-to-state.</p>
+
+<p>However, Bogart knows that merely opening up pathways to commercial opportunities isn’t enough.  
+Artists need business skills and step-by-step guidance to learn how to work professionally with buyers and industry leaders so she added business consulting, free articles, 
+and events that bring artists together with local business experts to the list of EC benefits.</p>
+  
+  <h4>Embrace Your Potential</h4>  
+
+<br>
+
+<p>A safe space to do business and sell artwork, EC is more than a typical retail and wholesale marketplace.  
+Through targeted articles and educational events, our Members meet each other, build important connections and learn things like;</p>
+
+<ul>
+    <li>Which sales opportunities are trustworthy?</li>
+    <li>How do I prepare for tax season?</li>
+    <li>What’s the best small business insurance for me?</li>
+    <li>How do I market myself and my art business?</li>
+    <li>What do I need to know about copyright protection and trademarks?</li>
+</ul>
+
+<p>And our EC Champions are available to answer questions, so find them in our <a href="members">Members</a> Page  then connect with them and send a private message! </p>
+<p>Embrace Creatives is where emerging and established creative businesses meet, sell, share ideas and strengthen our global arts community.</p>
+
+<center><a href="signup.php" class="btn btn-info" style="background-color:red" role="button" aria-pressed="true">JOIN US!</a></center>
+<hr>
+
+<h4>Why Embrace Creatives?</h4>
+
+<ul>
+    <li>Receive networking, exhibition and special event invitations.</li>
+    <li>Commercial buyer can purchase your art and product from individual images or collections.</li>
+    <li>Sell your artwork retail to your community and wholesale to commercial buyers like interior designers, architects and retailers.</li>
+    <li>Find over $1M in opportunities via calls & grants</li>
+    <li>Promote your business to attract clients and buyers.</li>
+    <li>Oh, and all passions are welcome – even nudes and erotic art</li>
+</ul>
+
+<p>As expressed in our Community Guidelines, Embrace Creatives gives you a safe, dedicated space to present your work, 
+find guidance and showcase your career with others around the globe!</p>
+
+<center><a href="#" class="btn btn-info" style="background-color:red" role="button" aria-pressed="true">PRESS</a></center>
+<br>
+<h3>WHO IS EMBRACE CREATIVES?</h3>
+
+<p>Embrace Creatives Founder & CEO - Andrea Bogart, professional artist, educator, speaker and business coach</p>
+
+<div class="text2">
+<h3 style="text-decoration: underline;">CHAMPIONS</h3>
+</div>
+
+      <!-- left side of the page-->
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+
+            <p>David Bogart<br>
+              Provides EC Members with small business insurance protection plus love and support to his wife, Andrea
+            </p>
+          
+            <p>Thomas Grubba<br>
+              Commercial and Fine Art Photographer & co-producer of EC Portfolio Reviews
+            </p>
+          
+            <p>Kiruba Sekaran<br>
+              Fine artist and SEO & SEM guide
+            </p>
+          
+            <p>Kelly O'Neill<br>
+              Sculptor and marketing & advertising master
+            </p>
+          
+            <p>Steve Czapiewski<br>
+              Fine artist and SEO & SEM guide
+            </p>
+          
+            <p>Karen Zuk<br>
+              Fine artist who welcomes every EC Member, brilliantly!
+            </p>
+          
+
+      
+          </div>
+      
+          <!-- right side of the page-->
+          <div class="col-sm">
+
+            <p>Steve Horstman<br>
+              Metro Detroit municipality support
+            </p>
+          
+            <p>Heather Meyer<br>
+              The Artsy Marketer / Branding &amp; Marketing for Artists supporting EC on the west side of Michigan
+            </p>
+          
+            <p>Fernando<br>
+              Art Director, Educator, Fine Artist and our first West Coast Ambassador!
+            </p>
+          
+            <p>Sue Schneider<br>
+              Multi-Media sculptor, web designer
+            </p>
+          
+            <p>Nancy Unger<br>
+              Sculptor and multi-media artist
+            </p></p>
+          </div>
+        </div>
+      </div>
+
+<hr>
+
+<h4>Get Involved</h4>
+
+<p>Lead a local Chapter as an EC Director<br>
+<a href="ambassador">Become a Digital Ambassador</a></p>
+
+</div>
+             
