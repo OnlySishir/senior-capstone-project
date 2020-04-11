095770aff269b667a9e03a43dae9371c74fd7aaa
Nadia313 <55292953+Nadia313@users.noreply.github.com>
2020-04-11 03:13:28 -0400

    Updated wholesale/retail price based on user quantity input

--- product.php
+++ product.php
@@ -1,3 +1,10 @@
+<style>
+hr.new4 {
+  border: 1px solid red;
+}
+
+</style>
+
 <div class="container">
 	<div class="card">
 		<div class="container-fliud">
@@ -31,33 +38,96 @@
 				</div>
 				<div class="details col-md-6" style="text-align: left;">
 					<h3 class="product-title"><?= $single['pro_title']?></h3>
-					<h6><span class="text-info">Seller:</span> <?= $single['firstname'].' '.$single['lastname']?></h6>
-					<h6>
-						<span class="text-info">Category Sort: </span> <?= $single['category_sort']?><br>
-						<span class="text-info">Category Name: </span> <?= $single['category_name']?>
-					</h6>
-					<h6>
-						<span class="text-info">Original Or Limited: </span> <?= $single['pro_original_limited']?><br>
-						<span class="text-info">Quantity available: </span> <?= $single['pro_quantity_stock']?>
-					</h6>
-					<p class="product-description">
-						<span class="text-info">Product Description:</span> <?= $single['pro_description']?>
+					<h6><span class="text-info">By </span> <?= $single['firstname'].' '.$single['lastname']?></h6><br>
+					<p>
+						<span class><strong>Category </strong></span> <?= $single['category_sort']?> , <?= $single['category_name']?>
+
+						<span style="float: right; width: 50%; text-align: left;"><strong>Taxable Yes/No </strong> <?= $single['pro_taxable']?></span> 				
+						<br><br><span><strong>Type </strong></span> <?= $single['category_name']?>
+						
+						<span style="float: right; width: 50%; text-align: left;"><strong>Min. Order</strong> <?= $single['pro_min_order']?></span>
+						<br><br><span><strong>Style #/SKU </strong> </span> <?= $single['pro_sku']?>
+
+						<span style="float: right; width: 50%; text-align: left;"><strong>Original/Limited Edition</strong> <?= $single['pro_original_limited']?></span> 
+
+
+						<br><br><span><strong>Weight </strong></span> <?= $single['pro_weight']?>
+						<span style="float: right; width: 50%; text-align: left;"><strong> Quantity/Stock </strong> <?= $single['pro_quantity_stock']?>	</span> 											
+						<br><br><span><strong>Materials/Medium </strong></span> <?= $single['pro_materials']?>						
+						<span style="float: right; width: 50%; text-align: left;"><strong> Custimizable </strong> <?= $single['pro_item_customizable']?></span>
+
+						<br><br><span></span> <strong>$<?= $single['pro_retail']?></strong>	
+					<?php if   ($this->session->certified =='ok') { 
+						if ($single['pro_wholesale'] != NULL) { ?>	
+						<span style="float: right; width: 50%; text-align: left;"><strong> Wholesale Price </strong> <?= $single['pro_wholesale']?></span>
+						<?php }
+						else { ?>
+						<span style="float: right; width: 50%; text-align: left;" hidden = true><strong> Wholesale Price </strong> <?= $single['pro_wholesale']?></span>
+						<?php } } ?>
+						
+					
+
 					</p>
-					<h6 class="product-material">
-						<span class="text-info">Materials/Medium:</span> <?= $single['pro_materials']?>
-					</h6>
-					<h4 class="price">Price: <span>$ <?= $single['pro_retail']?></span></h4>
-					<h4 class="price" style="text-decoration-line: line-through;">Wholesale Price: 
-						<span>$ <?= $single['pro_wholesale']?></span>
-					</h4>
-					<h5 class="sizes">Product Size: <?= $single['pro_size']?></h5>
-					<h6 class="product-material">Item is Custimizable: <?= $single['pro_item_customizable']?></h6>
-					<h6 class="colors">colors:
-						<span class=""><?= $single['pro_color']?></span>
-					</h6>
+					<p>
+					<h6>Description </h6><br>
+					<?= $single['pro_description']?>
+					</p>
+				
+										<div class="container">
+
+	
+	<?php if ($this->session->certified =='ok') { ?>
+		<input type="radio" name="optradio<?= $single['id']?>" value="<?= $single["pro_wholesale"]?>" onclick="doDisplay(this);"/>&nbsp;&nbsp;<strong>Buy Wholesale</strong>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<span id="wholesales" style="display:none">&nbsp;&nbsp;&nbsp;&nbsp;
+	<?php if ($single['pro_wholesale'] != NULL) { ?>
+			<br><input  class ='txtbx' size="10" type="number" id="txtNumber" value="<?php echo $single['pro_quantity_stock']?>"  disabled />
+	<?php }
+	else { ?>
+			<br><input  class ='txtbx' size="10" type="text" id="txtNumber" value="Not available"  disabled />
+	<?php }?>
+			</span>
+			<input type="radio" name="optradio<?= $single['id']?>" value="<?= $single["pro_retail"]?>" onclick="doDisplay(this);"/>&nbsp;&nbsp;<strong>Buy Retail</strong>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<span id="retails" style="display:none">&nbsp;&nbsp;&nbsp;&nbsp;
+			<br><br><center>Qty: <input name="qty" class ='txtbx1' size="10" type="number" id="txtNumber1" min="1" value="1" step="1"
+                      onkeypress='return event.charCode >= 48 && event.charCode <= 57' /></center>
+			</span>
+			</div>
+			<br />
+			<br />
+
+				<?php  $price = $this->session->certified =='ok' ? $single["pro_wholesale"] : $single["pro_retail"]; ?> 
+						<a href="#" class="btn btn-info btn-normal" onclick="add_cart_func(<?= $single['id']?>, 
+																							'<?= $single["pro_title"]?>', 
+																							'<?= $price?>')">
+      						<span class="glyphicon glyphicon-shopping-cart"></span> ADD TO CART</a>
+						<input type="hidden" name="real-price<?= $single['id']?>" value="<?= $single["pro_retail"]?>">
+						
+					<?php } 
+					
+					 else { ?>
+					 <br>
 					<h6 class="colors">Qty:
-						<input type="number" name="qty" min="1" value="1">
 					</h6>
+					<input type="radio" name="optradio<?= $single['id']?>" value="<?= $single["pro_wholesale"]?>" onclick="doDisplay(this);" hidden=true/>&nbsp;
+			<span id="wholesales" style="display:none">&nbsp;&nbsp;&nbsp;&nbsp;
+	<?php if ($single['pro_wholesale'] != NULL) { ?>
+			<input  class ='txtbx' size="10" type="number" id="txtNumber" value="<?php echo $single['pro_quantity_stock']?>"  disabled />
+	<?php }
+	else { ?>
+			<input  class ='txtbx' size="10" type="text" id="txtNumber" value="Not available"  disabled />
+	<?php }?>
+			</span>
+			<br />
+			<input type="radio" name="optradio<?= $single['id']?>" value="<?= $single["pro_retail"]?>" id ="radio" onclick="doDisplay(this);" hidden=true/>
+			<span id="retails" style="display:none">
+			<input name="qty" class ='txtbx1' size="10" type="number" id="txtNumber1" min="1" value="1" step="1"
+                      onkeypress='return event.charCode >= 48 && event.charCode <= 57' />
+			</span>
+			<br />
+			<br />
+
 					<h5>
 						<?php $price = $this->session->certified=='ok' ? $single["pro_wholesale"] : $single["pro_retail"]; ?>
 						<a href="#" class="btn btn-info btn-normal" onclick="add_cart_func(<?= $single['id']?>, 
@@ -65,19 +135,24 @@
 																							'<?= $price?>')">
       						<span class="glyphicon glyphicon-shopping-cart"></span> ADD TO CART
     					</a>
+						<input type="hidden" name="real-price<?= $single['id']?>" value="<?= $single["pro_retail"]?>">
+						
 					</h5>
+					<?php } ?>
 				</div>
 			</div>
 		</div>
-
+</div>
 	</div>
 </div>
 <!-- </div> -->
 <br>
 <br>
 <script type="text/javascript">
-	function add_cart_func(id, name, price){
-		let qty = $("input[name='qty']").val();
+	function add_cart_func(id, name){
+		// let qty = $("input[name='qty']").val();
+		let qty = $("#"+type).val();
+		var price = $("input[name='real-price"+id+"']").val();
 		jQuery.ajax({
 			url: '<?= base_url("product/add_to_cart")?>',
 			data: {'id':id, 'name':name, 'price': price, 'qty': qty},
@@ -94,3 +169,40 @@
 		});
 	}
 </script>
+
+
+
+<script type="text/javascript">
+    $('input[name="optradio<?= $single['id']?>"]').on('change', function(e){
+    $('input[name="real-price<?= $single['id']?>"]').val( $(this).val() );
+    });
+</script>
+
+
+<script>
+var type = "txtNumber";
+function doDisplay(radio)
+{
+    switch (radio.value)
+    {
+        case "<?= $single["pro_wholesale"]?>":
+            document.getElementById("wholesales").style.display = "inline";
+			document.getElementById("retails").style.display = "none";
+			type = "txtNumber"; 
+			break;
+
+        case "<?= $single["pro_retail"]?>":
+            document.getElementById("wholesales").style.display = "none";
+			document.getElementById("retails").style.display = "inline";
+			type = "txtNumber1"; 
+			break;
+	}
+
+}
+</script>
+
+<script>
+document.getElementById('radio').dispatchEvent(new MouseEvent("click"));
+</script>
+
+
